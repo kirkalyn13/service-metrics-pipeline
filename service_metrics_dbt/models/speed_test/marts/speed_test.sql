8d1c5d1f1cf7f1d92e5c5ce8a8a84cc264cad8c0
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-05-27 03:42:39 -0400

    fix: speed test mart ref

--- service_metrics_dbt/models/speed_test/marts/speed_test.sql
+++ service_metrics_dbt/models/speed_test/marts/speed_test.sql
@@ -6,6 +6,6 @@ SELECT
     AVG(download_speed_mbps)        AS download_speed_mbps,
     AVG(download_latency_ms)        AS download_latency_ms,
     AVG(idle_latency_ms)            AS idle_latency_ms
-FROM public.raw_speed_test
+FROM {{ ref('stg_speed_test') }}
 GROUP BY 1, 2, 3, 4
 ORDER BY 1 DESC
